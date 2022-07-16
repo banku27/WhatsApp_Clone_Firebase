5b001da4ee2ffc3fa2ed967110fc50512408fe54
banku27 <pankajparihar123321@gmail.com>
2022-07-17 00:48:21 +0530

    image picker

--- lib/features/auth/screens/user_information_screen.dart
+++ lib/features/auth/screens/user_information_screen.dart
@@ -1,5 +1,8 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:whatsapp_ui/common/utils/utils.dart';
 
 class UserInformationScreen extends ConsumerStatefulWidget {
   static const String routeName = '/user-information';
@@ -11,8 +14,67 @@ class UserInformationScreen extends ConsumerStatefulWidget {
 }
 
 class _UserInformationScreenState extends ConsumerState<UserInformationScreen> {
+  final TextEditingController nameController = TextEditingController();
+  File? image;
+
+  @override
+  void dispose() {
+    super.dispose();
+    nameController.dispose();
+  }
+
+  void selectImage() async {
+    image = await pickImageFromGallery(context);
+    setState(() {});
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    final size = MediaQuery.of(context).size;
+    return Scaffold(
+      body: SafeArea(
+          child: Center(
+        child: Column(
+          children: [
+            Stack(
+              children: [
+                image == null
+                    ? const CircleAvatar(
+                        backgroundImage: NetworkImage(
+                            'https://www.pngall.com/wp-content/uploads/5/Profile-PNG-Images.png'),
+                        radius: 60,
+                      )
+                    : CircleAvatar(
+                        backgroundImage: FileImage(image!),
+                        radius: 60,
+                      ),
+                Positioned(
+                  bottom: -10,
+                  left: 80,
+                  child: IconButton(
+                      onPressed: selectImage,
+                      icon: const Icon(Icons.add_a_photo)),
+                ),
+              ],
+            ),
+            Row(
+              children: [
+                Container(
+                  width: size.width * 0.85,
+                  padding: const EdgeInsets.all(20),
+                  child: TextField(
+                    controller: nameController,
+                    decoration: const InputDecoration(
+                      hintText: 'Enter your name',
+                    ),
+                  ),
+                ),
+                IconButton(onPressed: () {}, icon: const Icon(Icons.done))
+              ],
+            )
+          ],
+        ),
+      )),
+    );
   }
 }

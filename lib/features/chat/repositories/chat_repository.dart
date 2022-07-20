ac15851f0cc8d0c4b202eb3a807ddac0e3753397
banku27 <pankajparihar123321@gmail.com>
2022-07-20 23:53:14 +0530

    chat contact model

--- lib/features/chat/repositories/chat_repository.dart
+++ lib/features/chat/repositories/chat_repository.dart
@@ -1,6 +1,8 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/cupertino.dart';
+import 'package:whatsapp_ui/common/utils/utils.dart';
+import 'package:whatsapp_ui/models/user_model.dart';
 
 class ChatRepository {
   final FirebaseFirestore firestore;
@@ -11,11 +13,35 @@ class ChatRepository {
     required this.auth,
   });
 
+  void _saveDataToContactsSubCollection(
+    UserModel senderUserData,
+    UserModel recieverUserData,
+    String text,
+    DateTime timeSent,
+    String recieverUserId,
+  ) {}
+
   void sendTextMessage({
     required BuildContext context,
     required String text,
     required String receiverUserId,
+    required UserModel senderUser,
   }) async {
     // users -> sender id -> reciever id -> messages-> message id -> store messages
+
+    try {
+      var timeSent = DateTime.now();
+      UserModel receiverUserData;
+
+      var userDataMap =
+          await firestore.collection('users').doc(receiverUserId).get();
+
+      receiverUserData = UserModel.fromMap(userDataMap.data()!);
+
+      // users-> reciever user id-> chats -> current user id -> set data
+
+    } catch (e) {
+      showSnackBar(context: context, content: e.toString());
+    }
   }
 }

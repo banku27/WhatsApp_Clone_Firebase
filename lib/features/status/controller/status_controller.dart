af074b706be10a6a4af877f50c98eca2c97e9629
banku27 <pankajparihar123321@gmail.com>
2022-08-14 00:45:19 +0530

    status seen

--- lib/features/status/controller/status_controller.dart
+++ lib/features/status/controller/status_controller.dart
@@ -5,6 +5,7 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:whatsapp_ui/features/auth/controller/auth_controller.dart';
 
 import 'package:whatsapp_ui/features/status/repository/status_repository.dart';
+import 'package:whatsapp_ui/models/status_model.dart';
 
 final statusControllerProvider = Provider((ref) {
   final statusRepository = ref.read(statusRepositoryProvider);
@@ -23,12 +24,18 @@ class StatusController {
   });
 
   void addStatus(File file, BuildContext context) {
-    ref.watch(userDataAuthProvider).whenData((value) =>
-        statusRepository.uploadStatus(
-            username: value!.name,
-            profilePic: value.profilePic,
-            phoneNumber: value.phoneNumber,
-            statusImage: file,
-            context: context));
+    ref.watch(userDataAuthProvider).whenData((value) {
+      statusRepository.uploadStatus(
+          username: value!.name,
+          profilePic: value.profilePic,
+          phoneNumber: value.phoneNumber,
+          statusImage: file,
+          context: context);
+    });
+  }
+
+  Future<List<Status>> getStatus(BuildContext context) async {
+    List<Status> statuses = await statusRepository.getStatus(context);
+    return statuses;
   }
 }

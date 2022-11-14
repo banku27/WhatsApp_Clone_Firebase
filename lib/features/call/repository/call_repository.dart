a31908dd272f9bd1c8dab7cde26ce00c42a9550d
banku27 <pankajparihar123321@gmail.com>
2022-11-14 16:14:58 +0530

    call model

---
+++ lib/features/call/repository/call_repository.dart
@@ -0,0 +1,33 @@
+import 'dart:io';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_contacts/flutter_contacts.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:uuid/uuid.dart';
+import 'package:whatsapp_ui/common/repositories/common_firebase_storage_repository.dart';
+import 'package:whatsapp_ui/common/utils/utils.dart';
+import 'package:whatsapp_ui/models/call.dart';
+
+final callRepositoryProvider = Provider(
+  (ref) => CallRepository(
+    firestore: FirebaseFirestore.instance,
+    auth: FirebaseAuth.instance,
+  ),
+);
+
+class CallRepository {
+  final FirebaseFirestore firestore;
+  final FirebaseAuth auth;
+  CallRepository({
+    required this.firestore,
+    required this.auth,
+  });
+
+  void makeCall(
+      Call senderCallData, BuildContext context, Call receiverCallData) async {
+    try {} catch (e) {
+      showSnackBar(context: context, content: e.toString());
+    }
+  }
+}
